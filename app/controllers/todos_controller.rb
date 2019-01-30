b7cdd0cfa57019bfdcfb7fe1bdf2b404c41ac148
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-30 10:59:02 -0300

    ADD: create method

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,4 +6,19 @@ class TodosController < ApplicationController
   def new
     @todo = Todo.new
   end
+
+  def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      redirect_to root_path, notice: 'To-do saved.'
+    else
+      redirect_to new_todo_path, alert: 'An error has occurred, please try again.'
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
+  post 'todos', to: 'todos#create'
   get 'todos/new', as: 'new_todo'
   root 'todos#index'
 end
