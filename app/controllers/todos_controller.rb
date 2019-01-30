ce3dd8e2ad1c8b0b69a21291f33731536261b061
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-30 11:07:28 -0300

    ADD: show method and view

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -16,6 +16,10 @@ class TodosController < ApplicationController
     end
   end
 
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
   private
 
   def todo_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
   post 'todos', to: 'todos#create'
   get 'todos/new', as: 'new_todo'
+  get 'todos/:id', to: 'todos#show', as: 'todo'
   root 'todos#index'
 end
