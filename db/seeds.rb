4994a281e5d884383fcd05e25a4b7a460fac6137
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-30 10:43:47 -0300

    ADD: index method and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Todo.destroy_all
+
+10.times do
+  Todo.create(description: 'whatever', completed: false)
+end
